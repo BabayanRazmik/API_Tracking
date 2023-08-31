78bbb7f4aa4194783cc76805b21905deed725485
Razmik Babayan <razmik.babayan@7smarts.com>
2023-08-31 15:54:50 +0400

    Add project files.

---
+++ API_Tracking/Data/IssueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using API_Tracking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Tracking.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options) :base(options)
+        {   
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
